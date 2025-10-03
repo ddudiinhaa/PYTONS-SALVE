4388828e8597e8f59e1dc439f019d1a0388a7ef8
ddudiinhaa <142030963+ddudiinhaa@users.noreply.github.com>
2025-10-03 12:00:01 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,17 @@
+print ("bom dia seguimores")
+
+print(5+3)
+print(5-2)
+print(5*3)
+print(10/2)
+print(3**20)
+
+idade=70
+if idade>=77:
+    print("maior de idade") 
+else:
+    print("menor de idade")
+
+    for i in range(5):
+        print("número:",i)
+
